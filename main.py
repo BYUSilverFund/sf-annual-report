d575613e24b142fb27d78f9aa6160b24758a32b7
Andrew Hall <andrewmartinhall2@gmail.com>
2025-05-08 22:52:01 -0600

    First pass finishing the annual report.

--- main.py
+++ main.py
@@ -3,8 +3,10 @@ import polars as pl
 import polars_ols as pls  # noqa: F401
 from great_tables import GT
 import os
+import matplotlib.pyplot as plt
+import seaborn as sns
 
-os.makedirs("results")
+os.makedirs("results", exist_ok=True)
 
 query = Query()
 
@@ -17,7 +19,9 @@ def compute_total_fund_performance(start_date: str, end_date: str) -> pl.DataFra
     fund_df = (
         pl.from_pandas(query.get_fund_df(start_date=start_date, end_date=end_date))
         .join(benchmark_df, on="caldt", how="left", suffix=("_bmk"))
-        .with_columns(pl.col("xs_div_return", "xs_return_bmk", "div_return").fill_null(0))
+        .with_columns(
+            pl.col("xs_div_return", "xs_return_bmk", "div_return").fill_null(0)
+        )
     )
 
     performance = (
@@ -54,7 +58,9 @@ def compute_total_fund_performance(start_date: str, end_date: str) -> pl.DataFra
         #     pl.col("alpha").mul(pl.lit(252)),
         # )
         .with_columns(
-            pl.col('total_return').sub(pl.col('total_return_bmk').mul(pl.col('beta'))).alias('alphas')
+            pl.col("total_return")
+            .sub(pl.col("total_return_bmk").mul(pl.col("beta")))
+            .alias("alphas")
         )
         # Ratios
         .with_columns(
@@ -91,7 +97,9 @@ def compute_fund_performance(fund: str, start_date: str, end_date: str) -> pl.Da
             query.get_portfolio_df(fund=fund, start_date=start_date, end_date=end_date)
         )
         .join(benchmark_df, on="caldt", how="left", suffix=("_bmk"))
-        .with_columns(pl.col("xs_div_return", "xs_return_bmk", "div_return").fill_null(0))
+        .with_columns(
+            pl.col("xs_div_return", "xs_return_bmk", "div_return").fill_null(0)
+        )
     )
 
     performance = (
@@ -128,7 +136,9 @@ def compute_fund_performance(fund: str, start_date: str, end_date: str) -> pl.Da
         #     pl.col("alpha").mul(pl.lit(252)),
         # )
         .with_columns(
-            pl.col('total_return').sub(pl.col('beta').mul(pl.col('total_return_bmk'))).alias('alpha')
+            pl.col("total_return")
+            .sub(pl.col("beta").mul(pl.col("total_return_bmk")))
+            .alias("alpha")
         )
         # Ratios
         .with_columns(
@@ -149,7 +159,7 @@ def compute_fund_performance(fund: str, start_date: str, end_date: str) -> pl.Da
             "tracking_error",
             "sharpe_ratio",
             "information_ratio",
-            "total_return_bmk"
+            "total_return_bmk",
         )
     )
 
@@ -174,8 +184,7 @@ def create_overall_fund_performance(start_date: str, end_date: str) -> None:
 
     results = pl.DataFrame(data)
     results = (
-        results
-        .select(
+        results.select(
             "fund",
             "total_return",
             "volatility",
@@ -206,7 +215,7 @@ def create_overall_fund_performance(start_date: str, end_date: str) -> None:
         .fmt_percent(["Total Return", "Volatility", "Tracking Error", "Alpha"])
         .fmt_number(["Beta", "Sharpe Ratio", "Information Ratio"])
         .opt_stylize(style=3, color="gray")
-        .save(f"results/overall_fund_performance_{end_date}.png")
+        .save(f"results/table_all_funds_{end_date}.png")
     )
 
 
@@ -229,8 +238,7 @@ def compute_fund_holdings_performance(
     )
 
     performance = (
-        holdings_df
-        .filter(pl.col('ticker').is_in(current_tickers))
+        holdings_df.filter(pl.col("ticker").is_in(current_tickers))
         .sort(["ticker", "caldt"])
         # Cummulative features
         .with_columns(
@@ -311,9 +319,129 @@ def create_holdings_performance(start_date: str, end_date: str) -> None:
             .fmt_percent(["Total Return", "Volatility", "Alpha", "Weight"])
             .fmt_number(["Beta"])
             .opt_stylize(style=3, color="gray")
-            .save(f"results/holdings_performance_{fund}_{end_date}.png")
+            .save(f"results/table_holdings_{fund}_{end_date}.png")
         )
 
+
+def create_total_fund_chart(start_date: str, end_date: str) -> None:
+    benchmark_df = pl.from_pandas(query.get_benchmark_df(start_date, end_date)).select(
+        "caldt", "return", "div_return", "xs_return", "xs_div_return"
+    )
+
+    fund_df = (
+        pl.from_pandas(query.get_fund_df(start_date=start_date, end_date=end_date))
+        .join(benchmark_df, on="caldt", how="left", suffix=("_bmk"))
+        .with_columns(pl.col("div_return").fill_null(0))
+    )
+
+    cummulative_returns = fund_df.sort("caldt").with_columns(
+        pl.col("return").add(1).cum_prod().sub(1).mul(100).alias("cummulative_return"),
+        pl.col("div_return")
+        .add(1)
+        .cum_prod()
+        .sub(1)
+        .mul(100)
+        .alias("cummulative_return_bmk"),
+    )
+
+    plt.figure(figsize=(10, 6))
+    sns.lineplot(
+        cummulative_returns.to_pandas(),
+        x="caldt",
+        y="cummulative_return",
+        label="Portfolio",
+        color="royalblue",
+        linewidth=2
+    )
+    sns.lineplot(
+        cummulative_returns.to_pandas(),
+        x="caldt",
+        y="cummulative_return_bmk",
+        label="Benchmark",
+        color="lightcoral",
+        linestyle="dashed",
+        linewidth=2
+    )
+    plt.title("Overall Fund Performance")
+    plt.xlabel(None)
+    plt.ylabel("Cummulative Compounding Returns (%)")
+    plt.savefig("results/line_chart_total_fund.png", dpi=300)
+
+def create_fund_charts(start_date: str, end_date: str) -> None:
+    benchmark_df = pl.from_pandas(query.get_benchmark_df(start_date, end_date)).select(
+        "caldt", "return", "div_return", "xs_return", "xs_div_return"
+    )
+    funds = ['grad', 'undergrad', 'quant', 'brigham_capital']
+    for fund in funds:
+
+        fund_df = (
+            pl.from_pandas(query.get_portfolio_df(fund=fund, start_date=start_date, end_date=end_date))
+            .join(benchmark_df, on="caldt", how="left", suffix=("_bmk"))
+            .with_columns(pl.col("div_return").fill_null(0))
+        )
+
+        cummulative_returns = fund_df.sort("caldt").with_columns(
+            pl.col("return").add(1).cum_prod().sub(1).mul(100).alias("cummulative_return"),
+            pl.col("div_return")
+            .add(1)
+            .cum_prod()
+            .sub(1)
+            .mul(100)
+            .alias("cummulative_return_bmk"),
+        )
+
+        plt.figure(figsize=(10, 6))
+        sns.lineplot(
+            cummulative_returns.to_pandas(),
+            x="caldt",
+            y="cummulative_return",
+            label=f"{fund.replace('_', ' ').title()}",
+            color="royalblue",
+            linewidth=2
+        )
+        sns.lineplot(
+            cummulative_returns.to_pandas(),
+            x="caldt",
+            y="cummulative_return_bmk",
+            label="Benchmark",
+            color="lightcoral",
+            linestyle="dashed",
+            linewidth=2
+        )
+        plt.title(f"{fund.replace('_', ' ').title()} Performance")
+        plt.xlabel(None)
+        plt.ylabel("Cummulative Compounding Returns (%)")
+        plt.savefig(f"results/line_chart_{fund}_{end_date}.png", dpi=300)
+
+def create_combined_funds_chart(start_date: str, end_date: str) -> None:
+    funds = sorted(['grad', 'undergrad', 'quant', 'brigham_capital'])
+    colors = sns.color_palette('mako', 4)
+
+    plt.figure(figsize=(10, 6))
+    for fund, color in zip(funds, colors):
+
+        fund_df = (
+            pl.from_pandas(query.get_portfolio_df(fund=fund, start_date=start_date, end_date=end_date))
+        )
+
+        cummulative_returns = fund_df.sort("caldt").with_columns(
+            pl.col("return").add(1).cum_prod().sub(1).mul(100).alias("cummulative_return"),
+        )
+
+        sns.lineplot(
+            cummulative_returns.to_pandas(),
+            x="caldt",
+            y="cummulative_return",
+            label=f"{fund.replace('_', ' ').title()}",
+            color=color,
+            linewidth=2,
+        )
+
+    plt.title("All Funds Performance")
+    plt.xlabel(None)
+    plt.ylabel("Cummulative Compounding Returns (%)")
+
+    plt.savefig(f"results/line_chart_combined_{end_date}.png", dpi=300)    
 
 if __name__ == "__main__":
     start_date = "2024-05-01"
@@ -321,3 +449,6 @@ if __name__ == "__main__":
 
     create_overall_fund_performance(start_date, end_date)
     create_holdings_performance(start_date, end_date)
+    create_total_fund_chart(start_date, end_date)
+    create_fund_charts(start_date, end_date)
+    create_combined_funds_chart(start_date, end_date)
